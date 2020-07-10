ce700e92b03d18f8ae5b4bfc15c962420709f5a8
Saurabh Choudhury <saurabhc@Saurabhs-MacBook-Pro.local>
2020-07-10 13:07:48 -0700

    done

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Image } from 'react-native';
 
   const calculateWinner = (squares) =>{
+    let space = 'N';
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -17,10 +18,14 @@ import { StyleSheet, Text, View, Button } from 'react-native';
       const [a, b, c] = lines[i];
       if ((squares[a]==='X' || squares[a]==='O') && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
+        
       }
+      if (squares[a]===' ')
+          space = ' '
     }
-    return ' ';
+    return space;
   }
+  
   
   class Game extends React.Component {
     constructor(props){
@@ -59,57 +64,101 @@ import { StyleSheet, Text, View, Button } from 'react-native';
       const history = this.state.history;
       const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
-      const moves = history.map((step,move) => {
-        const desc = move ? 
-          'Go to move #' + move :
-          'Go to game start';
-        return (
-         <Button key={move} onPress={()=> this.jumpTo(move)} title={desc}/>
-        )
-      });
       let status;
       if(winner==='X' || winner ==='O'){
         status = 'Winner ' + winner;
-      } else{
+      } else if(winner==='N'){
+        status = 'Draw';
+      } else {
         status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
       }
       return (
-        <View>
-          <View>
-          <View style={{flex: 1, flexDirection: 'row', top: 50}}>
-            <View style={styles.Square}><Button title={current.squares[0]} onPress={() => this.handleClick(0)}/></View>
-            <View style={styles.Square}><Button title={current.squares[1]} onPress={() => this.handleClick(1)}/></View>
-            <View style={styles.Square}><Button title={current.squares[2]} onPress={() => this.handleClick(2)}/></View>
+        <View style={{padding: 10, justifyContent: "center", paddingHorizontal: 10}}>
+          <View style={styles.menu}>
+            <TouchableHighlight onPress={()=> this.jumpTo(0)}><View><Image style={styles.menuImage} source={require("./new.png")}/></View></TouchableHighlight>
+            <TouchableHighlight onPress={()=> this.jumpTo(this.state.stepNumber-1)}><View><Image style={styles.menuImage} source={require("./undo.png")}/></View></TouchableHighlight>
           </View>
-          <View style={{flex: 1, flexDirection: 'row', top: 100}}>
-            <View style={styles.Square}><Button title={current.squares[3]} onPress={() => this.handleClick(3)}/></View>
-            <View style={styles.Square}><Button title={current.squares[4]} onPress={() => this.handleClick(4)}/></View>
-            <View style={styles.Square}><Button title={current.squares[5]} onPress={() => this.handleClick(5)}/></View>
+          <View style={{flexDirection: 'row', justifyContent: "center",}}>
+            <TouchableHighlight onPress={() => this.handleClick(0)}><View style={styles.Square}>{squareRender(current.squares[0])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(1)}><View style={styles.Square}>{squareRender(current.squares[1])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(2)}><View style={styles.Square}>{squareRender(current.squares[2])}</View></TouchableHighlight>
           </View>
-          <View style={{flex: 1, flexDirection: 'row', top: 150}}>
-            <View style={styles.Square}><Button title={current.squares[6]} onPress={() => this.handleClick(6)}/></View>
-            <View style={styles.Square}><Button title={current.squares[7]} onPress={() => this.handleClick(7)}/></View>
-            <View style={styles.Square}><Button title={current.squares[8]} onPress={() => this.handleClick(8)}/></View>
+          <View style={{flexDirection: 'row', justifyContent: "center",}}>
+            <TouchableHighlight onPress={() => this.handleClick(3)}><View style={styles.Square}>{squareRender(current.squares[3])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(4)}><View style={styles.Square}>{squareRender(current.squares[4])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(5)}><View style={styles.Square}>{squareRender(current.squares[5])}</View></TouchableHighlight>
           </View>
-        </View>
-            <View style={{width: 160, top: 220 }}><Text>{status}</Text></View>
-            <View style={{width: 160, top: 250 }}>{moves}</View>
+          <View style={{flexDirection: 'row', justifyContent: "center",}}>
+            <TouchableHighlight onPress={() => this.handleClick(6)}><View style={styles.Square}>{squareRender(current.squares[6])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(7)}><View style={styles.Square}>{squareRender(current.squares[7])}</View></TouchableHighlight>
+            <TouchableHighlight onPress={() => this.handleClick(8)}><View style={styles.Square}>{squareRender(current.squares[8])}</View></TouchableHighlight>
+          </View>
+          <View style={styles.Status}>
+            <Text style={styles.StatusText}>{status}</Text>
+          </View>
         </View>
       );
     }
   }
-
+  const squareRender = (squareText) =>{
+    if (squareText==='X'){
+      return <Image style={styles.SquareImage} source={require("./x.png")}/>;
+    }
+    else if (squareText==='O'){
+      return <Image style={styles.SquareImage} source={require("./o.png")}/>;
+    } else{
+      return <Text style={styles.SquareText}>{squareText}</Text>;
+    }
+      
+  }
+  
   const styles = StyleSheet.create({
     Square: {
-      backgroundColor: 'white',
-      fontSize: 24,
-      fontWeight: 'bold',
-      height: 34,
       marginRight: -1,
       marginTop: -1,
+      borderWidth: 1,
+      borderColor: 'black',
+      height: 100,
+      width: 100,
+      alignContent: 'center',
+      alignItems: "center",
+      backgroundColor: "white",
       padding: 0,
+    },
+    SquareText: {
+      fontSize: 64,
+      fontWeight: 'bold',
       textAlign: 'center',
-      width: 34,
+    },
+    SquareImage: {
+      width: 95,
+      height: 95,
+      alignContent: 'center',
+      padding: 0,
+      backgroundColor: "white",
+      alignItems: "center",
+    },
+    Status: {
+      flexDirection: 'row', 
+      justifyContent: "center",
+      paddingTop: 50,
+    },
+    StatusText: {
+      fontSize: 34,
+      fontWeight: 'bold',
+    },
+    menu: {
+      flexDirection: 'row', 
+      justifyContent: "flex-end",
+      paddingBottom: 30,
+      paddingTop: 10
+    },
+    menuImage: {
+      width: 50,
+      height: 50,
+      paddingRight: 20,
+    
+
     },
   });
 
